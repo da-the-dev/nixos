4a35e0fb1234bb09acd3d9d05e5e8e1d1d960fc2
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-06-01 23:06:24 +0300

    Added disco config

---
+++ disko.nix
@@ -0,0 +1,43 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+      	# CHANGE IF NECESSARY
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "256M";
+              content = {
+               type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            swap = {
+	      # CHANGE IF NECESSARY
+              size = "16G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
