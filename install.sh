e6c9ee7cac3a59d86541443e3e6f8632c545b444
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-06-02 11:49:56 +0300

    Removed shebang

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # RUN AS SUPER USER
 nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko.nix 
 nixos-install --flake github:da-the-dev/nixos/main#powerpc
