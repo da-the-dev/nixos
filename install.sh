47214061cc482c838a10d6e19d6203c81a1999b4
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-06-02 18:06:42 +0300

    Changed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# RUN AS SUPER USER
-nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko.nix 
-nixos-install --flake github:da-the-dev/nixos/main#powerpc
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko.nix 
+nixos-generate-config --root /mnt --show-hardware-config ./hardware-configuration.nix
+sudo nixos-install --flake .#powerpc
 
